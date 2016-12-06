ecafbda15df440f0cba3d4df5c66f9b6336d7c9e
sdlingjun <sdlingjun@aliyun.com>
2016-12-06 22:20:10 +0800

    struts modular commit at first

---
+++ zlgl/core/src/main/java/zdhz/wls/dao/SearchException.java
@@ -0,0 +1,13 @@
+package zdhz.wls.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
---
+++ zlgl/core/src/main/java/zdhz/wls/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package zdhz.wls.service.impl;
+
+import zdhz.wls.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
